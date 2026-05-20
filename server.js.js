9d60de3c7dd6d3b8262b8e34d8e94b8d1791cd54
axxcexxa <franklinleal4@gmail.com>
2026-05-20 11:01:53 -0500

    Update server.js.js

--- server.js.js
+++ server.js.js
@@ -258,7 +258,7 @@ db.serialize(() => {
             tipo_licencia: 'REGULAR',
             correo: 'giovannycas77@yahoo.com',
             foto_url: '/fotos/castiblanco.png',
-            foto_doc_url: '/fotos/Ccastiblanco.doc.png',
+            foto_doc_url: '/fotos/castiblanco.doc.png',
             fecha_nacimiento: '1976-08-07', // Formateado a YYYY-MM-DD
             sexo: 'M',
             estatura: `5'6"`,
